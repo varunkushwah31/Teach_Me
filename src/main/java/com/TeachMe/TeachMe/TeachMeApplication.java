8dd418faefe116c6410031e414e21fbbca47374a
Varun Kushwah <varun.kush3@gmail.com>
2026-06-09 02:59:23 +0530

    first commit

---
+++ src/main/java/com/TeachMe/TeachMe/TeachMeApplication.java
@@ -0,0 +1,13 @@
+package com.TeachMe.TeachMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TeachMe/TeachMe/TeachMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeachMe.TeachMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
